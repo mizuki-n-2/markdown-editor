6b2d1c1ea08ef2148bf69d8af9be4deb6274b76e
mizuki-n-2 <mizukizhangze@gmail.com>
2021-06-20 23:31:14 +0900

    add history and pagination

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import styled from "styled-components";
 import { Header } from "../components/header";
-import { getMemos, MemoRecord } from "../indexeddb/memos";
+import { getMemos, MemoRecord, getMemoPageCount } from "../indexeddb/memos";
 
 const { useState, useEffect } = React
 
@@ -17,12 +17,13 @@ const HeaderArea = styled.div`
 `
 
 const Wrapper = styled.div`
-  bottom: 0;
+  bottom: 3rem;
   left: 0;
   position: fixed;
   right: 0;
   top: 3rem;
   padding: 0 1rem;
+  overflow-y: scroll;
 `
 
 const Memo = styled.button`
@@ -47,12 +48,54 @@ const MemoText = styled.div`
   white-space: nowrap;
 `
 
-export const History: React.FC = () => {
+const Paging = styled.div`
+  bottom: 0;
+  height: 3rem;
+  left: 0;
+  line-height: 2rem;
+  padding: 0.5rem;
+  position: fixed;
+  right: 0;
+  text-align: center;
+`
+
+const PagingButton = styled.button`
+  background: none;
+  border: none;
+  display: inline-block;
+  height: 2rem;
+  padding: 0.5rem 1rem;
+
+  &:disabled {
+    color: silver;
+  }
+`
+
+interface Props {
+  setText: (text: string) => void
+}
+
+export const History: React.FC<Props> = (props) => {
+  const { setText } = props
   const [memos, setMemos] = useState<MemoRecord[]>([])
+  const [page, setPage] = useState(1)
+  const [maxPage, setMaxPage] = useState(1)
+  const history = useHistory()
 
   useEffect(() => {
-    getMemos().then(setMemos)
+    getMemos(1).then(setMemos)
+    getMemoPageCount().then(setMaxPage)
   }, [])
+
+  const canNextPage: boolean = page < maxPage
+  const canPrevPage: boolean = page > 1
+  const movePage = (targetPage: number) => {
+    if (targetPage < 1 || maxPage < targetPage) {
+      return
+    }
+    setPage(targetPage)
+    getMemos(targetPage).then(setMemos)
+  } 
 
   return (
     <>
@@ -65,12 +108,33 @@ export const History: React.FC = () => {
       </HeaderArea>
       <Wrapper>
         {memos.map(memo => (
-          <Memo key={memo.datetime}>
+          <Memo
+            key={memo.datetime}
+            onClick={() => {
+              setText(memo.text)
+              history.push("/editor")
+            }}
+          >
             <MemoTitle>{memo.title}</MemoTitle>
             <MemoText>{memo.text}</MemoText>
           </Memo>
         ))}
       </Wrapper>
+      <Paging>
+        <PagingButton
+          onClick={() => movePage(page - 1)}
+          disabled={!canPrevPage}
+        >
+          ＜
+        </PagingButton>
+        {page} / {maxPage}
+        <PagingButton
+          onClick={() => movePage(page + 1)}
+          disabled={!canNextPage}
+        >
+          ＞
+        </PagingButton>
+      </Paging>
     </>
   )
 }
